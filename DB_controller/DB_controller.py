d1908dad1200f93a06ffcf98afec9d487e838617
ChernyshVova <chernyshvova@gmail.com>
2017-10-06 14:42:57 +0300

    Построение обьекта, для работы с БД

--- DB_controller/DB_controller.py
+++ DB_controller/DB_controller.py
@@ -1,34 +1,35 @@
-from sqlalchemy import create_engine
+from  sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer,String,MetaData,ForeignKey,create_engine
+from sqlalchemy.orm import relationship,sessionmaker
 
-class DB_controller:
-    connector= create_engine("sqlite://frame.db")
-
-
-
+Base = declarative_base()
 
 
+class Klient(Base):
+    __tablename__ = 'Klient'
+    id = Column(Integer,primary_key=True)
 
 
+class Frame(Base):
+    __tablename__ = 'frame'
+    id = Column(Integer, primary_key=True)
+    path = Column(String)
+    klient_id = Column(Integer, ForeignKey('Klient.id'))
+    Frame = relationship("Klient", backref="frames")
+
+    def __init__(self,path):
+        self.path = path
 
 
+class Context:
+    def __init__(self,db_path):
+        self.engine = create_engine("sqlite:///"+db_path)
+        self.metadata = MetaData()
+        self.metadata.create_all(self.engine)
 
-#from sqlalchemy.ext.declarative import declarative_base
-#Base = declarative_base()
-#from sqlalchemy import Column, Integer, String
-
-#Base = declarative_base()
-#class User(Base):
- #   __tablename__= "User"
-  #  id = Column(Integer,primary_key="true")
-   # name = Column(String)
-    #fullname = Column(String)
-
-    #def __repr__(self):
-     #   return "<User(%r, %r)>" % (
-      #  self.name, self.fullname
-   #     )
-#
-
-
-
-
+    def AddKlient(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        new_klient = Klient()
+        session.add(new_klient)
+        session.commit()
