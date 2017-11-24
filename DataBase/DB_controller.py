fb9d4f5c23cc8a57fceef1aa6982b267eb0cd31a
ChernyshVova <chernyshvova@gmail.com>
2017-11-24 04:34:27 +0200

    Переделал для удобства работы с клиентом и сервером

--- DataBase/DB_controller.py
+++ DataBase/DB_controller.py
@@ -1,7 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.types import DECIMAL
 from sqlalchemy.types import TIMESTAMP
+from  sqlalchemy.dialects import mysql
+
 
 Base = declarative_base()
 
@@ -16,7 +19,7 @@ class Client(Base):
 class Frame(Base):
     __tablename__ = 'Frame'
     id = Column(Integer, primary_key=True)
-    time = Column(TIMESTAMP)
+    time = Column(DECIMAL)
     client_id = Column(Integer, ForeignKey('Client.id_client'))
     parent = relationship("Client", backref="frame")
 
@@ -55,7 +58,7 @@ class Context:
         new_frame = Frame(time)
         select_client = self.GetClient(clientIp)
         new_frame.parent = select_client
-        self.session.commit()
+        self.session.add(new_frame)
         self.EndSession()
 
     def AddRole(self, role, token):
@@ -83,8 +86,12 @@ class Context:
 
     def GetClient(self, ip):
         self.StartSession()
+        #query = "select * from Client where ip = '%s'"%ip
+        #select_cl = self.session.execute(query)
+
         select_client = self.session.query(Client).filter_by(ip=ip).first()
         self.EndSession()
+
         return select_client
 
     def GetAlLIP(self):
@@ -95,15 +102,18 @@ class Context:
 
     def GetRangeFrames(self, startTime, endTime, clientip):
         self.StartSession()
-        client_id = self.GetClient(clientip)
+        client_id = self.GetClient(clientip).id_client
+        #print(client_id)
 
-        convDateEnd = "STR_TO_DATE('" + startTime + "','%Y-%m-%d %H:%i:%s')"
-        convDateStart = "STR_TO_DATE('" + endTime + "','%Y-%m-%d %H:%i:%s')"
-        query = "select path from Frame where Frame.time BETWEEN %s and %s and Frame.client_id='%d'"%(convDateStart, convDateEnd, client_id.id)
-        print(query)
+        query = "select time from Frame where time BETWEEN %s and %s  and client_id='%d'"%(startTime,endTime, client_id)
+
         selectFrames = self.session.execute(query)
+        list_names = list()
+        for row in selectFrames:
+            list_names.append(str(row.time))
 
-        return selectFrames
+        self.EndSession()
+        return list_names
 
     def RoleInit(self, token):
         self.StartSession()
